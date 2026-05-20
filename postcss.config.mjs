9afe16fb99fa761aa3f454a3639ba95abc7d3d37
dotnetcarpenter <jon.ronnenberg@gmail.com>
2026-05-21 00:35:11 +0200

    glitch: introduce randomness

---
+++ postcss.config.mjs
@@ -0,0 +1,9 @@
+import random from "@csstools/postcss-random-function";
+
+// https://docs.astro.build/en/guides/styling/#postcss
+// https://github.com/postcss/postcss#usage
+export default {
+  plugins: [
+    random
+  ],
+};
